12f10451a5276cfbb9b4f00ba0695291bdd9d1f8
Kenny Nguyen <k239nguy@uwaterloo.ca>
2025-09-28 19:06:40 -0400

    have logs in scientific notation

--- src/Generator.cc
+++ src/Generator.cc
@@ -290,7 +290,7 @@ void Generator::ConstructGenerator_IrrepUnmixing() {
 
     new_Eta /= eta_norm + 1e-100;
 
-    std::cout << "Norm:\t|[G, H]| = " << std::setprecision(9) << commutator_norm << ";\t|Eta| = " << eta_norm << ";" << std::endl;
+    std::cout << std::scientific << std::setprecision(9) << "Norm:\t|[G, H]| = " << commutator_norm << ";\t|Eta| = " << eta_norm << ";" << std::endl;
 
     *Eta = std::move(new_Eta);
 }
