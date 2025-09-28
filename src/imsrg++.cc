bb117b4296951ffc0c30c293b895880c6c2019cc
Kenny Nguyen <k239nguy@uwaterloo.ca>
2025-09-28 18:42:19 -0400

    Reapply "fixed last typo"
    This reverts commit 7444c29ad389bebe969446e432e56d87f3aeadbe.

--- src/imsrg++.cc
+++ src/imsrg++.cc
@@ -1091,7 +1091,7 @@ int main(int argc, char** argv)
   }
 
   if(casimir != "") {
-    std::cout << "reading casimir: " << casimir << ";" std::endl;
+    std::cout << "reading casimir: " << casimir << ";" << std::endl;
     // might be modelspace_imsrg? all other instances used modelspace
     // G = -casmir operator from the Johnson paper
     Operator G = -rw.ReadOperator2b_Miyagi(casimir, modelspace);
