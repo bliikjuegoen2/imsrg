c8c9d1defba644612b61f64f93a3f8b9b402c218
Kenny Nguyen <k239nguy@uwaterloo.ca>
2025-09-28 18:41:55 -0400

    Revert "change formatting of logs"
    This reverts commit 14f44837a2d2f0007eca3c27b8c8078fe71f3d5d.

--- src/Generator.cc
+++ src/Generator.cc
@@ -290,7 +290,7 @@ void Generator::ConstructGenerator_IrrepUnmixing() {
 
     new_Eta /= eta_norm + 1e-100;
 
-    std::cout << "Norm:\t|[G, H]| = " << std::setprecision(9) << commutator_norm << ";\t|Eta| =" << eta_norm << ";" std::endl;
+    std::cout << "|[G, H]| =\t" << std::setprecision(9) << commutator_norm << "|Eta| =\t" << eta_norm << std::endl;
 
     *Eta = std::move(new_Eta);
 }
